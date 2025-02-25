96da7317292cae1808b8533c8a2921809bd8eaa5
HalilAlb <halil_3425@hotmail.com>
2025-02-25 12:14:17 +0300

    last update makefile

--- push_swap/main.c
+++ push_swap/main.c
@@ -5,16 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/24 16:46:15 by malbayra          #+#    #+#             */
-/*   Updated: 2025/02/24 18:45:39 by malbayra         ###   ########.fr       */
+/*   Created: 2025/02/25 10:43:09 by malbayra          #+#    #+#             */
+/*   Updated: 2025/02/25 12:13:08 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void check_args(int ac, char **av)
+{
+    int i;
+    int j;
+    
+    i = 0;
+    if(ac < 2)
+           write(10, "Erro\n", 6);
+    while(++i < ac)
+    {
+        j = 0;
+        while(av[i][j])
+        {
+            if(!ft_isdigit(av[i][j]) && av[i][j] != ' ')
+                write(2, "Err\n", 6);
+            j++;
+        }
+    }
+}
 
 int main(int ac,char **av)
 {
+    check_args(ac, av);
     
-     
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,14 +6,17 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 16:51:09 by malbayra          #+#    #+#             */
-/*   Updated: 2025/02/24 18:36:58 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/02/25 10:53:40 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "libft/libft.h"
 # include "libft/ft_printf/ft_printf.h"
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
 
 #endif
