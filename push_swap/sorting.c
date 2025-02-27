7583989b22f599df29cc61a436a02a5077919b59
HalilAlb <109541183+HalilAlb@users.noreply.github.com>
2025-02-27 19:37:37 +0300

    Update sorting.c

--- push_swap/sorting.c
+++ push_swap/sorting.c
@@ -60,3 +60,54 @@ void	radix_sort(t_stack **a, t_stack **b, int size)
 		}
 	}
 }
+
+
+/*
+1020
+void	operations(t_stack **a, t_stack **b, int bit_index)
+{
+	int	size;
+	int	index;
+
+	while (*b)
+		push(a, b, "pa");
+	size = stack_len(*a);
+	index = 0;
+	while (index < size)
+	{
+		if ((stack_sorted(*a) && *b == NULL))
+			break ;
+		if (get_bit(bit_index, (*a)->value))
+			rotate(a, NULL, "ra");
+		else
+			push(b, a, "pb");
+		index++;
+	}
+}
+
+void	radix_sort(t_stack **a, t_stack **b, int size)
+{
+	int	index;
+	int	max_bits;
+
+	if (size == 2)
+		swap(a, 0, "sa");
+	else if (size == 3)
+		sort_three(a);
+	else if (size == 4 || size == 5)
+		sort_five(a, b);
+	else
+	{
+		max_bits = 0;
+		while ((size - 1) >> max_bits)
+			max_bits++;
+		index = -1;
+		while (++index < max_bits)
+		{
+			operations(a, b, index);
+			if ((stack_sorted(*a) && *b == NULL))
+				break ;
+		}
+	}
+}
+*/
